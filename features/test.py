3b8f51ba5f897cc0e2865deb9b588fa7610e4188
vadosdu <tehpromoil@gmail.com>
2019-03-05 20:53:54 +0300

    Added first test

---
+++ features/test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("D:\Install programs\Python and Selenium\chromedriver")
+driver.get("https://wikipedia.org")
+
+search_field = driver.find_element_by_id("searchInput")
+search_field.send_keys("test text")
+
+search_button = driver.find_element_by_xpath("//*[@id='search-form']/fieldset/button")
+search_button.click()
+
+assert "test text" in driver.title
+driver.quit()
